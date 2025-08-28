b8d36a0c4feeb3859481607e85a2865a17e6ebf0
Samuel Malec <malec293@gmail.com>
2025-08-28 14:35:33 +0200

    feat: implement basic interpreter workflow

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,17 +1,58 @@
 package com;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.lexer.Lexer;
+import com.lexer.Token;
+import com.parser.Parser;
+import com.util.TLangFileValidator;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length != 1) {
+            System.err.println("Invalid number of arguments, please enter the path to a .tl file");
+            return;
+        }
+
+        Path path = getPath(args[0]);
+        if (path == null || !TLangFileValidator.isValidTLangFile(path)) {
+            printError("Invalid path: " + args[0]);
+            return;
+        }
+
+        String content = readFile(path);
+        if (content == null) return;
+        Lexer lexer = new Lexer(content);
+        List<Token> tokens = lexer.scanTokens();
+        Parser parser = new Parser(tokens);
+        parser.parse();
+    }
+
+    private static void printError(String s) {
+        System.err.println(s);
+    }
+
+    private static Path getPath(String arg) {
+        try {
+            return Paths.get(arg);
+        } catch (InvalidPathException ipe) {
+            return null;
+        }
+    }
+
+    private static String readFile(Path path) {
+        try {
+            return Files.readString(path);
+        } catch (IOException e) {
+            printError("An error has occurred while reading the file: " + path);
+            return null;
         }
     }
 }
