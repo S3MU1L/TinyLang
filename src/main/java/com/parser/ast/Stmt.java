2e837bde032d3bef90aafda305eb6695e0e4b814
Samuel Malec <malec293@gmail.com>
2025-08-28 14:35:21 +0200

    feat: implement statements

---
+++ src/main/java/com/parser/ast/Stmt.java
@@ -0,0 +1,103 @@
+package com.parser.ast;
+
+import java.util.List;
+
+public abstract class Stmt {
+
+    public interface Visitor<R> {
+        R visitLetStmt(Let stmt);
+        R visitExpressionStmt(Expression stmt);
+        R visitIfStmt(If stmt);
+        R visitWhileStmt(While stmt);
+        R visitReturnStmt(Return stmt);
+        R visitBlockStmt(Block stmt);
+    }
+
+    public abstract <R> R accept(Visitor<R> visitor);
+
+    public static class Let extends Stmt {
+        public final String name;
+        public final Expr initializer;
+
+        public Let(String name, Expr initializer) {
+            this.name = name;
+            this.initializer = initializer;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLetStmt(this);
+        }
+    }
+
+    public static class Expression extends Stmt {
+        public final Expr expression;
+
+        public Expression(Expr expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitExpressionStmt(this);
+        }
+    }
+
+    public static class If extends Stmt {
+        public final Expr condition;
+        public final Stmt thenBranch;
+        public final Stmt elseBranch;
+
+        public If(Expr condition, Stmt thenBranch, Stmt elseBranch) {
+            this.condition = condition;
+            this.thenBranch = thenBranch;
+            this.elseBranch = elseBranch;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitIfStmt(this);
+        }
+    }
+
+    public static class While extends Stmt {
+        public final Expr condition;
+        public final Stmt body;
+
+        public While(Expr condition, Stmt body) {
+            this.condition = condition;
+            this.body = body;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitWhileStmt(this);
+        }
+    }
+
+    public static class Return extends Stmt {
+        public final Expr value;
+
+        public Return(Expr value) {
+            this.value = value;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitReturnStmt(this);
+        }
+    }
+
+    public static class Block extends Stmt {
+        public final List<Stmt> statements;
+
+        public Block(List<Stmt> statements) {
+            this.statements = statements;
+        }
+
+        @Override
+        public <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBlockStmt(this);
+        }
+    }
+}
