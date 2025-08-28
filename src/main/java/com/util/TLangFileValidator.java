bfe7c7fd485d0e2b76dcfb3d8bcafd965219a8a5
Samuel Malec <malec293@gmail.com>
2025-08-28 14:34:38 +0200

    feat: tlang file validator utility

---
+++ src/main/java/com/util/TLangFileValidator.java
@@ -0,0 +1,12 @@
+package com.util;
+
+import java.nio.file.Path;
+
+public class TLangFileValidator {
+    
+    private static final String TLANG_SUFFIX = ".tl";
+
+    public static boolean isValidTLangFile(Path path)  {
+        return (path.getFileName().toString().endsWith(TLANG_SUFFIX));
+    }
+}
