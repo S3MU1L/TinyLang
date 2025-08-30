29bfd3869abd28685c3c2da1a0c94e395971695a
Samuel Malec <malec293@gmail.com>
2025-08-30 20:36:09 +0200

    feat: create Environment

---
+++ src/main/java/com/tinylang/Environment.java
@@ -0,0 +1,46 @@
+package com.tinylang;
+
+import com.tinylang.error.RuntimeError;
+import com.tinylang.token.Token;
+
+import java.util.HashMap;
+
+public class Environment {
+
+    private final Environment enclosing;
+    private final HashMap<String, Object> values = new HashMap<>();
+
+    public Environment() {
+        this.enclosing = null;
+    }
+
+    public Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
+
+    public void define(String name, Object value) {
+        values.put(name, value);
+    }
+
+    public void assign(Token name, Object value) {
+        if (values.containsKey(name.lexeme())) {
+            values.put(name.lexeme(), value);
+            return;
+        }
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme() + "'.");
+    }
+
+    Object get(Token name) {
+        if (values.containsKey(name.lexeme())) {
+            return values.get(name.lexeme());
+        }
+        if (enclosing != null) {
+            return enclosing.get(name);
+        }
+        throw new RuntimeError(name, "Undefined variable '" + name.lexeme() + "'.");
+    }
+}
